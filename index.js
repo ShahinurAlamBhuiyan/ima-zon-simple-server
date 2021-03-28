84402f936303ba8cf125573b8335f9994333c223
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2021-03-28 15:20:06 +0600

    created root api

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(cors())
 
 const port = 4000
 
+app.get('/',(req, res)=>{
+    res.send("hello am from db, it's working ")
+})
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const productsCollection = client.db("imaJohnStore").collection("products");
